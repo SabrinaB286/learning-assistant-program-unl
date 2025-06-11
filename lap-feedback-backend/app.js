c8314cf3e6b5bde12a179ceee2f6a1b44be2f144
SabrinaB286 <111913314+SabrinaB286@users.noreply.github.com>
2025-06-11 09:15:44 -0500

    Fixed dynamic storage

--- lap-feedback-backend/app.js
+++ lap-feedback-backend/app.js
@@ -10,13 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+console.log('Applying middleware...');
 app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
+console.log('Registering routes...');
 app.use('/staff', staffRoutes);
 app.use('/feedback', feedbackRoutes);
-
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
